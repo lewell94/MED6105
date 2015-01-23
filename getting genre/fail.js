c1a0b06d9d63711f2c496640c615c85872e24622
Elliot Lewis <lewell94@gmail.com>
2015-01-23 15:16:02 +0000

    first commit

---
+++ getting genre/fail.js
@@ -0,0 +1,21 @@
+//Spotify - Not all artists have genres, also how to access?
+
+/*function artistSearch() {
+	$.get('https://api.spotify.com/v1/search?q=' + searchValue + '&type=artist', function(data) {
+        console.log(data)
+        $(data.artists.items).each(function(){
+            $('body').append(this.streamable);
+        });
+    })
+};*/
+
+//iTunes - How to get it to work
+
+/*function artistSearch() {
+	$.get('https://itunes.apple.com/search?term=jack+johnson&entity=musicArtist', function(data) {
+        console.log(data)
+        $(data.artist).each(function(){
+            $('body').append(this.streamable);
+        });
+    })
+};*/
---
+++ getting genre/script.js
@@ -0,0 +1,46 @@
+/* Trying it with three different services. At first used last.fm because had used it before - couldn't get it to drill down. Then tried spotify - see problems, then tried itunes - see problems - 15th jan. 
+
+Finally got last.fm to work with the getTagName function and forEach, next big step - check the genre matches, this might be easier when genre is stored in db? done! create the genreValue var at top - akin to db save - and run the check in the getTagName function (which is completely changed from earlier), before checking the value of the doesGenreMatch variable. PROBLEM = user created tags cause problems - a great big pile of leaves dont have emo on. how to fix? also moved spotify and itunes into fail.js to clean up. - 19th jan */
+
+//What do when no genre? Also A Great Big Pile of Leaves not having emo?
+
+$(document).ready(function(){    
+    $('#genre-save').click(function(){
+        genreValue = document.getElementById("genre-select").value;
+    })
+});
+
+function genreCheck() {
+	$.get('https://ws.audioscrobbler.com/2.0/?method=artist.getInfo&artist=' + searchValue + '&api_key=2fc90f462963de3f1aa5889f105418ec&format=json', function(data) {
+        $(data.artist).each(function(){
+            tagList = this.tags.tag
+            if (tagList === undefined){
+                $('body').append('no genre');
+            } else {
+                doesGenreMatch = null
+                function getTagName(element, index, array) {
+                    //$('body').append('<p>' + element.name + ': </p>');
+                    if(element.name === genreValue) {
+                        //$('body').append('<p style="border-bottom:1px solid black;">yay</p>');
+                        doesGenreMatch = 'yes'
+                    } else {    
+                        //$('body').append('<p style="border-bottom:1px solid black;">nay</p>');
+                    }
+                }
+                tagList.forEach(getTagName);
+                if(doesGenreMatch === 'yes'){
+                    $('body').append('<p>Yay! ' + searchValue + ' is a ' + genreValue + ' band</p>');
+                } else {
+                    $('body').append('<p>Nay! ' + searchValue + ' is not a ' + genreValue + ' band</p>');
+                }
+            }
+        })
+    })
+};
+
+$(document).ready(function(){    
+    $('#submit').click(function(){
+        searchValue = document.getElementById("search").value;
+        genreCheck();
+    })
+});
